dfc7a07804de67899db87c03d16372350f1ac8cd
Rynstheoverlord <107300355+Rynstheoverlord@users.noreply.github.com>
2024-03-13 17:31:02 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,45 +1,47 @@
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
-import time
+import requests, time
+import json
+import smtplib
 
-st.markdown("""
-# Dreamscape **Images**🌌
+
+def feedback_message():
+  st.success("Feedback sent!", icon="✅")
+  
+
+st.markdown("""# DreamScape🌌
 Here creativity takes shape
+
 
 
 """)
 
-st.page_link("Home.py", label=":blue[Go home]")
 
-search_term = st.text_input("Search for an image")
+st.page_link("pages/Images.py", label=":blue[click here to search for a specific image here]🖼️")
 
-url = rf'https://www.google.no/search?q={search_term}&client=opera&hs=cTQ&source=lnms&tbm=isch&sa=X&safe=active&ved=0ahUKEwig3LOx4PzKAhWGFywKHZyZAAgQ_AUIBygB&biw=1920&bih=982'
 
-query = requests.get(url)
+def generate_random():
+  for i in range(5):
+    st.image(f"https://picsum.photos/200/300?random={i}", use_column_width=True)
 
-soup = BeautifulSoup(query.content, 'html.parser')
+    # download button
+    st.download_button("Download📩", requests.get(f"https://picsum.photos/500/600?random={i}").content, "dreamscape.png", mime="image/png", key=f"{i}")
 
-thumbnails = soup.find_all('img')
 
-images = []
-image_id = 0
+# expander for random images
+with st.expander("Random images"):
+  generate_random()
+  
 
-for image in thumbnails:
-    src = image.attrs["src"]
-    if "https://" in src:
-      images.append(src)
-    
+# expander for getting a particular image
 
-if st.button("search", use_container_width=True, type="primary"):
-  with st.empty():
-    progressbar = st.progress(0)
-    for i in range(100):
-      progressbar.progress(i + 1)
-      time.sleep(0.02)
-      
-    with st.expander(f"{search_term} images:"):
-      for image in images:
-        image_id += 1
-        st.image(image)
-        st.download_button(f"Download image {image_id}📩", requests.get(image).content, "dreamscape.png", mime="image/png")
+with st.expander("Get specific image"):
+  search = st.number_input("Enter image id", min_value=1)
+  
+  if st.button("search", use_container_width=True):
+    with st.empty():
+      progressbar = st.progress(0)
+      for i in range(100):
+        progressbar.progress(i + 1)
+        time.sleep(0.02)
+      st.write("image:")
+    st.image(f"https://picsum.photos/id/{int(search)}/200/300", use_column_width=True)
